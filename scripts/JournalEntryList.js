c073080d504407b64bf4111b51e94c1b5b4e3c7a
nathanRomad <nathan.a.hamilton@gmail.com>
2021-02-17 11:07:25 -0600

    not rendering journalEntries.. need second set of eyes/HELP

--- scripts/JournalEntryList.js
+++ scripts/JournalEntryList.js
@@ -1,25 +1,17 @@
-/*
-The list component, just like Martin's Aquarium, will take all of the individual entry HTML strings and inject it into the browser DOM.
- *  Purpose:
- *    To render as many journal entry components as
- *    there are items in the collection exposed by the
- *    data provider component
- */
 import { useJournalEntries } from "./JournalDataProvider.js"
 import { JournalEntryComponent } from "./JournalEntry.js"
 
-// DOM reference to where all entries will be rendered
-const entryLog = document.querySelector(".entryLog")
+const journalEntryContainer = document.querySelector(".journalEntryContainer")
 
 export const EntryListComponent = () => {
-    // Use the journal entry data from the data provider component
+    debugger
+    let journalEntryHTMLrepresentation = ""
     const entries = useJournalEntries()
     for (const entry of entries) {
-        JournalEntryComponent(entry)
-        /*
-            Invoke the component that returns an
-            HTML representation of a single entry
-        */
-        entryLog.innerHTML += ???
+        journalEntryHTMLrepresentation += JournalEntryComponent(entry)
     }
+    journalEntryContainer.innerHTML += `
+        <h2>Journal Entries</h2>
+        ${journalHTMLrepresentation}
+        `
 }
